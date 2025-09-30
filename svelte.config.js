97ccc018536f0cad41a10de980ff8017b52d1b32
Ivo Ferreira <126669984+IvoMCFerreira@users.noreply.github.com>
2025-09-30 20:14:57 +0100

    a

--- svelte.config.js
+++ svelte.config.js
@@ -1,21 +1,16 @@
-import adapter from '@sveltejs/adapter-auto';
+import adapter from '@sveltejs/adapter-static';
 import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
-	// Consult https://svelte.dev/docs/kit/integrations
-	// for more information about preprocessors
-	preprocess: vitePreprocess(),
+    preprocess: vitePreprocess(),
 
-	kit: {
-		// adapter-auto only supports some environments, see https://svelte.dev/docs/kit/adapter-auto for a list.
-		// If your environment is not supported, or you settled on a specific environment, switch out the adapter.
-		// See https://svelte.dev/docs/kit/adapters for more information about adapters.
-		adapter: adapter(),
-		paths: {
-      		base: '', // Replace with your repo name if deploying to a subfolder
-    	}
-	}
+    kit: {
+        adapter: adapter(),
+        paths: {
+            base: '' // Use your repo name here if deploying to a subfolder
+        }
+    }
 };
 
 export default config;
